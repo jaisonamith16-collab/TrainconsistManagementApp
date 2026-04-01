389edfb6e41c423eb3d97b4db25efc17d747ab2e
jaisonamith16-collab <jaisonamith16@gmail.com>
2026-04-01 09:40:44 +0530

    UC1: Add welcome message module

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -26,51 +26,16 @@ public class TrainConsistManagementApp {
         bogieList.add(new Bogie("Sleeper", 72));
         bogieList.add(new Bogie("AC Chair", 56));
         bogieList.add(new Bogie("First Class", 24));
+        bogieList.add(new Bogie("Sleeper", 70));
 
-        List<Bogie> filteredBogies = bogieList.stream()
-                .filter(b -> b.getCapacity() > 60)
-                .collect(Collectors.toList());
+        Map<String, List<Bogie>> groupedBogies = bogieList.stream()
+                .collect(Collectors.groupingBy(Bogie::getName));
 
-        for (Bogie b : filteredBogies) {
-            System.out.println(b.getName() + " - Capacity: " + b.getCapacity());
-        }
-    }
-}
-import java.util.*;
-        import java.util.stream.*;
-
-class Bogie {
-    private String name;
-    private int capacity;
-
-    public Bogie(String name, int capacity) {
-        this.name = name;
-        this.capacity = capacity;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getCapacity() {
-        return capacity;
-    }
-}
-
-public class TrainConsistManagementApp {
-    public static void main(String[] args) {
-        List<Bogie> bogieList = new ArrayList<>();
-
-        bogieList.add(new Bogie("Sleeper", 72));
-        bogieList.add(new Bogie("AC Chair", 56));
-        bogieList.add(new Bogie("First Class", 24));
-
-        List<Bogie> filteredBogies = bogieList.stream()
-                .filter(b -> b.getCapacity() > 60)
-                .collect(Collectors.toList());
-
-        for (Bogie b : filteredBogies) {
-            System.out.println(b.getName() + " - Capacity: " + b.getCapacity());
+        for (Map.Entry<String, List<Bogie>> entry : groupedBogies.entrySet()) {
+            System.out.println(entry.getKey() + ":");
+            for (Bogie b : entry.getValue()) {
+                System.out.println("  Capacity: " + b.getCapacity());
+            }
         }
     }
 }
